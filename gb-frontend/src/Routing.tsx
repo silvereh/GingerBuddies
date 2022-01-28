85dec544f69d46820c6a1c04469349f91dbadc06
silvereh <silvere.heraudeau@gmail.com>
2022-01-28 10:13:09 -0700

    Fixed Errors in template

--- gb-frontend/src/Routing.tsx
+++ gb-frontend/src/Routing.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 
+import { Home } from './Home';
+
 const Routing = () => (
   <Routes data-testid="Routing">
-    <Route path="/" element={  } />
+    <Route path="/" element={ <Home /> } />
   </Routes>
 );
 
